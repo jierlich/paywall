87471a49749dc0dc73d515756f5c1e4a7c7f714b
jierlich <jierlich@umich.edu>
2021-06-10 01:43:07 -0400

    [CONFIG] install additional dev dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
+require("@nomiclabs/hardhat-waffle");
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  defaultNetwork: "hardhat",
+  // Add test networks here when ready for easy deployment
+  // Should main net go here as well? It seems a bit
+  // fast and loose in terms of deployment practices.
+  // networks: {},
   solidity: "0.7.3",
 };
