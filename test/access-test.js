66030cdc50c5a081bd2f0e081b02ae4f9abbf2ac
jierlich <jierlich@umich.edu>
2021-06-16 17:42:44 -0400

    [feat] add test for create function return value

--- test/access-test.js
+++ test/access-test.js
@@ -5,15 +5,24 @@ const { ethers } = require("hardhat")
 const ASSET_CREATED = "AssetCreated"
 
 describe("Access", () => {
+    const bnOne = new ethers.BigNumber.from(1)
+    const bnHundred = new ethers.BigNumber.from(100)
+
     beforeEach(async () => {
         const AccessContract = await ethers.getContractFactory("Access")
         this.access = await AccessContract.deploy()
+        this.signers = await ethers.getSigners()
         await this.access.deployed()
     })
 
     it("Should create an accessable asset", async () => {
-        const [assetOwner, buyer, nonBuyer] = await ethers.getSigners()
-        const createCall = await this.access.create(new ethers.BigNumber.from(100), assetOwner.address)
-        await expect(createCall).to.emit(this.access, ASSET_CREATED).withArgs(new ethers.BigNumber.from(1), assetOwner.address)
+        // Static call verifies correct return value without altering state
+        const createCallStatic = await this.access.callStatic.create(bnHundred, this.signers[0].address)
+        await expect(createCallStatic).to.equal(bnOne)
+
+        // Non-static call changes state and checks emitted event
+        const createCall = await this.access.create(bnHundred, this.signers[0].address)
+        await expect(createCall).to.emit(this.access, ASSET_CREATED)
+            .withArgs(new ethers.BigNumber.from(1), this.signers[0].address)
     })
 })
